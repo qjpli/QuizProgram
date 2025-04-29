182ad66ca33b373f94261badf657155634ba18ae
bitebuds <bitebuds.proj@gmail.com>
2025-04-30 00:23:53 +0800

    Commit

---
+++ lib/customs/fields/custom_text_field.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quizprogram/globals.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final String? hintText;
+  final IconData? prefixIcon;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final ValueChanged<String>? onChanged;
+  final Color? fillColor;
+  final Color? borderColor;
+  final Color? iconColor;
+  final bool isPassword;
+  final String? labelText;
+  final TextStyle? labelStyle;
+  final FocusNode? focusNode; // Added FocusNode
+
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    this.hintText,
+    this.prefixIcon,
+    this.keyboardType,
+    this.obscureText = false,
+    this.validator,
+    this.onChanged,
+    this.fillColor,
+    this.borderColor,
+    this.iconColor,
+    this.isPassword = false,
+    this.labelText,
+    this.labelStyle,
+    this.focusNode, // Added FocusNode
+  });
+
+  @override
+  _CustomTextFormFieldState createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  late bool _obscureText;
+  bool isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      focusNode: widget.focusNode,
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+          setState(() {
+            isFocused = true;
+          });
+        } else {
+          setState(() {
+            isFocused = false;
+          });
+        }
+      },
+      child: TextFormField(
+        controller: widget.controller,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: screenSize * 0.013
+          ),
+          labelText: widget.labelText != null
+              ? widget.controller.text.isNotEmpty || isFocused
+              ? widget.labelText
+              : widget.hintText
+              : null,
+          labelStyle: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: screenSize * 0.013
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          alignLabelWithHint: false,
+          prefixIcon: widget.prefixIcon != null
+              ? Icon(widget.prefixIcon, color: widget.iconColor)
+              : null,
+          suffixIcon: widget.isPassword
+              ? IconButton(
+            icon: Icon(
+              _obscureText ? Icons.visibility : Icons.visibility_off,
+              color: widget.iconColor ?? Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+          )
+              : null,
+          fillColor: widget.fillColor ?? const Color(0xFFf2f2f2),
+          filled: true,
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: widget.borderColor ?? Colors.transparent),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: widget.borderColor ?? Colors.transparent),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: widget.borderColor ?? const Color(0xFFE5E5E5)),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          contentPadding: EdgeInsets.symmetric(
+              vertical: screenHeight * 0.018,
+              horizontal: screenWidth * 0.04
+          ),
+        ),
+        keyboardType: widget.keyboardType,
+        obscureText: _obscureText,
+        validator: widget.validator,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
---
+++ lib/screens/auth/sign_up_1.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:quizprogram/globals.dart';
+
+import '../../customs/fields/custom_text_field.dart';
+
+class SignUp1 extends StatefulWidget {
+  const SignUp1({super.key});
+
+  @override
+  State<SignUp1> createState() => _SignUp1State();
+}
+
+class _SignUp1State extends State<SignUp1> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      FlutterNativeSplash.remove();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.05
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: screenHeight * 0.08),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Image.asset('assets/images/general/app-logo-1.png',
+                      width: screenWidth * 0.3,
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.025 ),
+                  Text('Creating Account',
+                    style: TextStyle(
+                        fontSize: screenSize * 0.016,
+                        fontWeight: FontWeight.w400
+                    ),
+                  ),
+                  Text('Start your quizzer account today',
+                    style: TextStyle(
+                        fontSize: screenSize * 0.012,
+                        color: const Color(0xFF808080),
+                        fontWeight: FontWeight.w300
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight * 0.04),
+                  CustomTextFormField(
+                    controller: _emailController,
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.02),
+                  CustomTextFormField(
+                    controller: _passwordController,
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onChanged: (value) {
+
+                    },
+                    isPassword: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.04),
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF313235),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        fixedSize: Size(
+                            screenWidth * 0.9,
+                            screenHeight * 0.06
+                        )
+                    ),
+                    child: Text('Sign up',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.07),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'Already have an account? ',
+                            style: TextStyle(
+                                color: const Color(0xFF808080),
+                                fontSize: screenSize * 0.012,
+                                fontWeight: FontWeight.w300
+                            ),
+                        ),
+                        TextSpan(
+                            text: 'Sign in',
+                            style: TextStyle(
+                                color: const Color(0xFF313235),
+                                fontSize: screenSize * 0.012,
+                                fontWeight: FontWeight.w500
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print('Clicked');
+                                Get.back();
+                              }
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
