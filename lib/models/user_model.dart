f279fc3817389457938d8cc8dad01caa450bd276
bitebuds <bitebuds.proj@gmail.com>
2025-04-29 22:37:06 +0800

    Frist Commit

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,28 +3,32 @@ import 'dart:convert';
 class UserFields {
   static const String id = 'id';
   static const String name = 'name';
-  static const String email = 'email';
+  static const String username = 'username';
+  static const String password = 'password';
   static const String createdAt = 'created_at';
 }
 
 class UserModel {
   final String id;
   final String name;
-  final String email;
+  final String username;
+  final String password;
   final DateTime? createdAt;
 
   const UserModel({
     required this.id,
     required this.name,
-    required this.email,
-    this.createdAt, // Optional
+    required this.username,
+    required this.password,
+    this.createdAt,
   });
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       id: map[UserFields.id] as String? ?? '',
       name: map[UserFields.name] as String? ?? '',
-      email: map[UserFields.email] as String? ?? '',
+      username: map[UserFields.username] as String? ?? '',
+      password: map[UserFields.password] as String? ?? '',
       createdAt: map[UserFields.createdAt] != null
           ? DateTime.tryParse(map[UserFields.createdAt])
           : null,
@@ -35,12 +39,14 @@ class UserModel {
     return {
       UserFields.id: id,
       UserFields.name: name,
-      UserFields.email: email,
+      UserFields.username: username,
+      UserFields.password: password,
       UserFields.createdAt: createdAt?.toIso8601String(),
     };
   }
 
   String toJson() => json.encode(toMap());
 
-  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
 }
