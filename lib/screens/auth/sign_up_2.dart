db85477c7ff39022d9241c54426838e071a7e4f0
JOHN_PAUL_IBALLA <qjpliballa@tip.edu.ph>
2025-04-30 04:42:52 +0000

    Sign up

--- lib/screens/auth/sign_up_2.dart
+++ lib/screens/auth/sign_up_2.dart
@@ -2,12 +2,18 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import 'package:quizprogram/globals.dart';
+import 'package:quizprogram/providers/auth_user_provider.dart';
+import 'package:quizprogram/providers/user_provider.dart';
 
 import '../../customs/fields/custom_text_field.dart';
 
 class SignUp2 extends StatefulWidget {
-  const SignUp2({super.key});
+  final String username;
+  final String password;
+
+  const SignUp2({super.key, required this.username, required this.password});
 
   @override
   State<SignUp2> createState() => _SignUp2State();
@@ -27,11 +33,12 @@ class _SignUp2State extends State<SignUp2> {
 
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    final authUserProvider = Provider.of<AuthUserProvider>(context);
+
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.05
-        ),
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -41,23 +48,24 @@ class _SignUp2State extends State<SignUp2> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Center(
-                    child: Image.asset('assets/images/general/app-logo-1.png',
+                    child: Image.asset(
+                      'assets/images/general/app-logo-1.png',
                       width: screenWidth * 0.3,
                     ),
                   ),
-                  SizedBox(height: screenHeight * 0.025 ),
-                  Text('Choose your name',
+                  SizedBox(height: screenHeight * 0.025),
+                  Text(
+                    'Choose your name',
                     style: TextStyle(
                         fontSize: screenSize * 0.016,
-                        fontWeight: FontWeight.w400
-                    ),
+                        fontWeight: FontWeight.w400),
                   ),
-                  Text("What should we call you?",
+                  Text(
+                    "What should we call you?",
                     style: TextStyle(
                         fontSize: screenSize * 0.012,
                         color: const Color(0xFF808080),
-                        fontWeight: FontWeight.w300
-                    ),
+                        fontWeight: FontWeight.w300),
                   )
                 ],
               ),
@@ -71,6 +79,7 @@ class _SignUp2State extends State<SignUp2> {
                     labelText: 'Username',
                     hintText: 'Enter your username',
                     keyboardType: TextInputType.visiblePassword,
+                    onChanged: (val) => setState(() {}),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Email is required';
@@ -80,24 +89,32 @@ class _SignUp2State extends State<SignUp2> {
                   ),
                   SizedBox(height: screenHeight * 0.04),
                   ElevatedButton(
-                    onPressed: () {
+                    onPressed: _nameController.text.trim().isNotEmpty
+                        ? () async {
+                            final status = await userProvider.createUser(
+                                name: _nameController.text.trim(),
+                                username: widget.username,
+                                password: widget.password);
 
-                    },
+                            if (status != null) {
+                              final setAuth =
+                                  await authUserProvider.setAuthUser(status);
+
+                              print('Account Successfully Created');
+                            }
+                          }
+                        : null,
                     style: ElevatedButton.styleFrom(
                         backgroundColor: const Color(0xFF313235),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        fixedSize: Size(
-                            screenWidth * 0.9,
-                            screenHeight * 0.06
-                        )
-                    ),
-                    child: Text('Sign up',
+                        fixedSize:
+                            Size(screenWidth * 0.9, screenHeight * 0.06)),
+                    child: Text(
+                      'Done',
                       style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600
-                      ),
+                          color: Colors.white, fontWeight: FontWeight.w600),
                     ),
                   ),
                   SizedBox(height: screenHeight * 0.07),
@@ -109,22 +126,19 @@ class _SignUp2State extends State<SignUp2> {
                           style: TextStyle(
                               color: const Color(0xFF808080),
                               fontSize: screenSize * 0.012,
-                              fontWeight: FontWeight.w300
-                          ),
+                              fontWeight: FontWeight.w300),
                         ),
                         TextSpan(
                             text: 'Sign in',
                             style: TextStyle(
                                 color: const Color(0xFF313235),
                                 fontSize: screenSize * 0.012,
-                                fontWeight: FontWeight.w500
-                            ),
+                                fontWeight: FontWeight.w500),
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
                                 print('Clicked');
                                 Get.back();
-                              }
-                        )
+                              })
                       ],
                     ),
                   )
